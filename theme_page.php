a525686b5cd4385890c2dd0c17ae5d7cf3ae2040
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-09 01:15:05 +0300

    Update theme_page.php

--- theme_page.php
+++ theme_page.php
@@ -109,7 +109,7 @@ if (!(empty($page_confirmed['popover']))):
 
 echo "</amp-sidebar>";
 
-echo "<span on='tap:popover.toggle' role='button' class='material-icons menu_button'>menu</span>";
+echo "<span on='tap:popover.toggle' role='button' class='material-icons menu_button background_2'>menu</span>";
 
 
 if (!(empty($page_confirmed[$page_temp]['body'])) || !(empty($gallery))):
