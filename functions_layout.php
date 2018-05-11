6414da9e8bfcb086ac6771d2ddc9e0faf6d7149c
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-11 13:40:18 +0300

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -136,9 +136,10 @@ function amp_header($title=null, $canonical=null) {
 	if (empty($login)): echo "<div id='navigation-signin-button'><a href='/account/'><i class='material-icons'>account_box</i> Sign In</a></div>"; endif;
 	if (!(empty($login))):
 		echo "<div id='navigation-settings-button'><a href='/account/'><i class='material-icons'>settings</i> Settings</a></div>";
-		$time_temp = date("Y m d h:i", $login['cookie_time']+86400);
-		echo "<div id='navigation-loggedin-time'>Logged in ".$time_temp." more ".$time_unit_temp."</div>";
-
+		if ( (time() - $login['cookie_time']) >= 82800 );
+			echo "<div id='navigation-loggedin-time'>Automatic logout soon</div>";
+			endif;
+	
 		echo "<div id='navigation-add-button'><a href='/create/'><i class='material-icons'>note_add</i> Create</a></div>";
 
 		endif;
