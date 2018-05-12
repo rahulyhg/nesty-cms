48d080ffc94ace96cac467ee53447f54094777a1
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-12 14:31:04 +0300

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -3,20 +3,16 @@ admin_bar($login,"home");
 
 $content_array = [];
 
-$sql_temp = "SELECT page_id, created_time, header FROM $database.pages ORDER BY created_time DESC LIMIT 10";
+$sql_temp = "SELECT page_id, created_time, header FROM $database.pages ORDER BY created_time DESC LIMIT 2";
 foreach($connection_pdo->query($sql_temp) as $row):
-	$content_array[] = "{{{".$row['page_id']."}{".$row['header']."}{tile}}}";
+	echo body_process("{{{".$row['page_id']."}{".$row['header']."}{tile}}}");
 	endforeach;
 
-$sql_temp = "SELECT media_id FROM $database.media ORDER BY datetime_original DESC LIMIT 10"; // datetime_process is alternative method of sorting
+$sql_temp = "SELECT media_id FROM $database.media ORDER BY datetime_original DESC LIMIT 5"; // datetime_process is alternative method of sorting
 foreach($connection_pdo->query($sql_temp) as $row):
-	$content_array[] = "[[[".$row['media_id']."]]]";
+	echo body_process("[[[".$row['media_id']."][large]]]");
 	endforeach;
 
-if (!(empty($content_array))):
-	shuffle($content_array);
-	$content_array = implode("\n\n", $content_array);
-	echo body_process($content_array);
-	endif;
+echo "<a href='/schedule/'>view more</a>";
 
 footer(); ?>
