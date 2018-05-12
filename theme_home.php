ccde62e693312f1f952a50eb28b9d62c758d49fa
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-12 14:52:40 +0300

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -10,17 +10,17 @@ if (!(empty($description))):
 $count_temp = 0;
 $sql_temp = "SELECT page_id, created_time, header FROM $database.pages ORDER BY created_time DESC LIMIT 3";
 foreach($connection_pdo->query($sql_temp) as $row):
-	if ($count_temp == 0): echo "<h2>Recent posts</h2>"; $count_temp++; endif;
+	if ($count_temp == 0): echo "<span class='home-header'>Recent posts</span>"; $count_temp++; endif;
 	echo body_process("{{{".$row['page_id']."}{".$row['header']."}{tile}}}");
 	endforeach;
 
 $count_temp = 0;
 $sql_temp = "SELECT media_id FROM $database.media ORDER BY datetime_original DESC LIMIT 3"; // datetime_process is alternative method of sorting
 foreach($connection_pdo->query($sql_temp) as $row):
-	if ($count_temp == 0): echo "<h2>Latest images</h2>"; $count_temp++; endif;
+	if ($count_temp == 0): echo "<span class='home-header'>Latest images</span>"; $count_temp++; endif;
 	echo body_process("[[[".$row['media_id']."][large]]]");
 	endforeach;
 
-echo "<a href='/schedule/'>view more</a>";
+echo "<span class='home-header'><a href='/schedule/'>view more</a></span>";
 
 footer(); ?>
