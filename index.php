0512ea2af890e02557bb85deb9665dc50874bf3b
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-13 15:57:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -154,6 +154,23 @@ if ($page_temp == "schedule"):
 // options to create
 if (!(empty($login)) && ($page_temp == "create")):
 	html_header("Create");
+
+	$sql_temp = "SELECT COUNT(*) AS count FROM $database.pages";
+	$count_pages = $connection_pdo->prepare($sql_temp);
+	$count_pages->execute();
+	$result = $count_pages->fetchAll();
+	foreach ($result as $row):
+		print_r($row);
+		endforeach;
+
+	$sql_temp = "SELECT COUNT(*) AS count FROM $database.entries";
+	$count_entries = $connection_pdo->prepare($sql_temp);
+	$count_entries->execute();
+	$result = $count_entries->fetchAll();
+	foreach ($result as $row):
+		print_r($row);
+		endforeach;
+
 	echo "<div id='create-window'>";
 	echo "<a href='/new/'><div id='create-window-new-page-button' class='background_1'>new page</div></a>";
 	echo "<a href='/create/'><div id='create-window-add-entry-button' class='background_2'>add entry</div></a>";
