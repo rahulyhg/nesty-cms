19b66a33ed25adfb2b28ca4681ed72570bc0a9b6
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-13 15:40:02 +0300

    Update api_entries.php

--- api_entries.php
+++ api_entries.php
@@ -3,17 +3,15 @@ $retrieve_page = $connection_pdo->prepare($sql_temp);
 $retrieve_page->execute();
 $result = $retrieve_page->fetchAll();
 foreach ($result as $row):
-        	$information_array[$row['page_id']] = [
-					"entry_id" => $row['entry_id'],
-					"page_id" => $row['page_id'],
-			"link" => "https://".$domain."/".$row['entry_id']."/",
-		"name" => json_decode($row['name'], true),
-		"alternate_name" => json_decode($row['alternate_name'], true),
-		"summary" => [],
-		"appendix" => $appendix_temp,
-		"parents" => [],
-		"children" => [] ];
-		endforeach;
+
+print_r($row); exit;
+	$information_array[$row['page_id']] = [
+	"entry_id" => $row['entry_id'],
+	"page_id" => $row['page_id'],
+	"link" => "https://".$domain."/".$row['entry_id']."/",
+	"name" => json_decode($row['name'], true),
+	"summary" => null ];
+	endforeach;
     
  print_r(json_encode($information_array));
 
