aa007ed4ef05ddc3430b841a5501cba74d2cdc91
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-05-21 10:16:58 +0300

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -306,7 +306,7 @@ function body_process($body_incoming) {
 			$image_string = "<a href='".$media_info[$media_id_temp]['link']."' on='tap:lightbox".$media_id_temp."' role='button' tabindex='1'>view image</a>";
 		elseif ($filename_size == "large"):
 			$image_string = "<div class='image_large'>";
-			$image_string .= "<figure><amp-img on='tap:lightbox".$media_id_temp."' src='".$media_info[$media_id_temp]['link']."large/' width='".$img_width_large."px' height='".$img_height_large."px' role='button' tabindex='1' sizes='(min-width: 1000px) 900px, (min-width: 500px) 90vw'></amp-img>";	
+			$image_string .= "<figure><amp-img on='tap:lightbox".$media_id_temp."' src='".$media_info[$media_id_temp]['link']."large/' width='".$img_width_large."px' height='".$img_height_large."px' role='button' tabindex='1' sizes='(min-width: 1100px) 1000px, (min-width: 500px) 90vw, 90vw'></amp-img>";	
 			if (!(empty($file_description))): $image_string .= "<amp-fit-text width='".($img_width_large)."px' height='30px' min-font-size='14px' max-font-size='14px'>".mb_substr(strip_tags(str_replace(["</th>", "</td>", "</div>", "</p>", "<br>", "<br />"], ' ',$file_description)),0,200)."</amp-fit-text>"; endif;
 			$image_string .= "</figure>";
 			$image_string .= "<a href='".$media_info[$media_id_temp]['link']."' target='_blank'><div class='image_open_link material-icons'>open_in_new</div></a>";
