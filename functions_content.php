d0951775875e1e1688777ce20d3c43ec89a8f07e
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2018-06-14 19:25:15 +0300

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -377,10 +377,17 @@ function body_process($body_incoming) {
 		$citation_string[] = "<a href='https://".$entry_info[$citation_id_temp]['domain']."/e/".$citation_id_temp."/'><div class='citation-credit background_".rand(1,10)."'>".$entry_info[$citation_id_temp]['publisher']." &nbsp;|&nbsp; ".$citation_id_temp."</div></a>";
 
 		$citation_date_string = [];
-		if (!(empty($entry_info[$citation_id_temp]['year']))): $citation_date_string[] = $entry_info[$citation_id_temp]['year']; endif;
-		if (!(empty($entry_info[$citation_id_temp]['month']))): $citation_date_string[] = date("F", strtotime("2000-".$entry_info[$citation_id_temp]['month']."-01")); endif;
-		if (!(empty($entry_info[$citation_id_temp]['day']))): $citation_date_string[] = $entry_info[$citation_id_temp]['day']; endif;
-		if (!(empty($citation_date_string))): $citation_string[] = "<div class='citation-name'>".implode(" ", $citation_date_string)."</div>"; endif;
+		if (!(empty($entry_info[$citation_id_temp]['year']))):
+			$citation_date_string[] = $entry_info[$citation_id_temp]['year'];
+			endif;
+		if (!(empty($entry_info[$citation_id_temp]['month']))):
+			$citation_date_string[] = date("F", strtotime("2000-".$entry_info[$citation_id_temp]['month']."-01"));
+			if (!(empty($entry_info[$citation_id_temp]['day']))):
+				$citation_date_string[] = date("jS", strtotime("2000-01-01".$entry_info[$citation_id_temp]['day']));
+				endif; endif;
+		if (!(empty($citation_date_string))):
+			$citation_string[] = "<div class='citation-credit'>".implode(" ", $citation_date_string)."</div>";
+			endif;
 
 		if (!(empty(login)) && ($domain == $entry_info[$citation_id_temp]['domain'])):
 			$citation_string[] = "<a href='/e/".$citation_id_temp."/edit/'><div class='citation-edit'>Edit</div></a>"; endif;
